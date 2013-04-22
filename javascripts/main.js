c34d740b28543ab716dc76b6b5f5e9d29038928c
xuechong87 <xuechong87@gmail.com>
2013-04-22 22:11:12 +0800

    fix

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,9 +9,9 @@
     }).done(function (data) {
         var gallery = $('#gallery'),
             url;
-        $.each(data.contents, function (index, data) {
-			for(var i = 0;i<data[index].pic.length;i++){
-				url = "/photo/" + data[index].dir + "/" + data[index].pic[i];
+        $.each(data.contents, function (index, content) {
+			for(var i = 0;i<content.pic.length;i++){
+				url = "/photo/" + content.dir + "/" + content.pic[i];
 				$('<a data-gallery="gallery"/>')
                 .append($('<img>').prop('src', url))
                 .prop('href', url)
