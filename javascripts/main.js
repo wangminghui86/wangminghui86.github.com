df278a853b467c23382b7d55e8bd0872a2e5e31f
xuechong87 <xuechong87@gmail.com>
2013-04-22 22:01:05 +0800

    fix

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,13 +9,15 @@
     }).done(function (data) {
         var gallery = $('#gallery'),
             url;
-        $.each(data.contents.pics, function (index, data) {
-            url = "/photo/" + content.dir + "/"
-            $('<a data-gallery="gallery"/>')
+        $.each(data.contents, function (index, data) {
+			for(var i = 0;i<data[index].pic.length){
+				url = "/photo/" + data[index].dir + "/" + data[index].pic[i];
+				$('<a data-gallery="gallery"/>')
                 .append($('<img>').prop('src', url))
                 .prop('href', url)
                 .prop('title', "title")
                 .appendTo(gallery);
+			}
         });
     });
 });
